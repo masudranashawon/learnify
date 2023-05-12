e48145ce020e3adb98825d25388fb1397ba2e8f4
masudranashawon <mohammadmasudrana3396@gmail.com>
2023-05-12 19:05:25 +0600

    navbar added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='footer bg-black py-5 text-center text-gray-400'>
+      &copy; {new Date().getFullYear()} Learnify. All rights reserved.
+    </footer>
+  );
 };
 
 export default Footer;
