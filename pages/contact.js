68512890168fba7829e1342a30469df20191d06f
masudranashawon <mohammadmasudrana3396@gmail.com>
2023-05-22 00:53:20 +0600

    emailjs integration complete

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,39 @@
-import SectionHeader from "@/components/SectionHeader";
+import { useRef } from "react";
 import Image from "next/image";
+import SectionHeader from "@/components/SectionHeader";
+import emailjs from "@emailjs/browser";
 import { IoMdSend } from "react-icons/io";
 
 const ContactPage = () => {
+  const formRef = useRef(null);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    /* EMAILJS INTEGRATION */
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_SERVICE_ID,
+        process.env.NEXT_PUBLIC_TEMPLATE_ID,
+        formRef.current,
+        process.env.NEXT_PUBLIC_USER_ID
+      )
+      .then(
+        () => {
+          console.log("Message sent successfully!");
+        },
+        () => {
+          console.log("Failed to send message. Please try again.");
+        }
+      );
+
+    /* RESET FORM */
+    e.target.querySelector(".name").value = "";
+    e.target.querySelector(".number").value = "";
+    e.target.querySelector(".email").value = "";
+    e.target.querySelector(".message").value = "";
+  };
+
   return (
     <div className='contact wrapper py-10'>
       <SectionHeader
@@ -14,27 +45,32 @@ const ContactPage = () => {
       <div className='flex flex-col lg:flex-row gap-10 mt-10'>
         <div className='w-full lg:w-1/2 flex justify-center p-5'>
           <Image
+            className='w-full object-cover rounded-lg'
             src='https://images.pexels.com/photos/7709296/pexels-photo-7709296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
             alt='Some man are connected on call in call center'
             width={500}
             height={500}
-            className='w-full object-cover rounded-lg'
           />
         </div>
 
         <div className='w-full lg:w-1/2'>
-          <form className='contact-form flex flex-col gap-5'>
+          <form
+            onSubmit={sendEmail}
+            ref={formRef}
+            className='contact-form flex flex-col gap-5'
+          >
             <div className='row grid grid-cols-1 lg:grid-cols-2 gap-5'>
               <div className='form-control w-full flex flex-col gap-2'>
                 <label htmlFor='name' className='cursor-pointer'>
                   Name
                 </label>
                 <input
+                  className='name border border-gray-300 focus:border-gray-600 px-4 py-3 rounded-lg outline-none duration-300'
                   type='text'
                   name='name'
                   id='name'
                   placeholder='Sarah Parker'
-                  className='name border border-gray-300 focus:border-gray-600 px-4 py-3 rounded-lg outline-none duration-300'
+                  required
                 />
               </div>
 
@@ -43,11 +79,12 @@ const ContactPage = () => {
                   Phone Number
                 </label>
                 <input
+                  className='number border border-gray-300 focus:border-gray-600 px-4 py-3 rounded-lg outline-none duration-300'
                   type='tel'
                   name='number'
                   id='number'
                   placeholder='+88 01XXXXXXXXX'
-                  className='number border border-gray-300 focus:border-gray-600 px-4 py-3 rounded-lg outline-none duration-300'
+                  required
                 />
               </div>
             </div>
@@ -57,11 +94,12 @@ const ContactPage = () => {
                 Email Address
               </label>
               <input
+                className='email border border-gray-300 focus:border-gray-600 px-4 py-3 rounded-lg outline-none duration-300'
                 type='email'
                 name='email'
                 id='email'
                 placeholder='sara@example.com'
-                className='email border border-gray-300 focus:border-gray-600 px-4 py-3 rounded-lg outline-none duration-300'
+                required
               />
             </div>
 
@@ -70,12 +108,13 @@ const ContactPage = () => {
                 Message
               </label>
               <textarea
+                className='message border border-gray-300 focus:border-gray-600 h-32 rounded-xl outline-none py-3 px-4 resize-none duration-300'
                 placeholder='Type your message here...'
                 name='message'
                 id='message'
                 rows='1'
                 cols='50'
-                className='message border border-gray-300 focus:border-gray-600 h-32 rounded-xl outline-none py-3 px-4 resize-none duration-300'
+                required
               />
             </div>
 
