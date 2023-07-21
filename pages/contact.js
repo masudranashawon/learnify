2604f0876ce9923b6ddd5d802e683f3c8d0a9f9a
masudranashawon <mohammadmasudrana3396@gmail.com>
2023-07-21 21:25:28 +0600

    contact animation added

--- pages/contact.js
+++ pages/contact.js
@@ -62,7 +62,15 @@ const ContactPage = () => {
       />
 
       <div className='flex flex-col lg:flex-row gap-10 mt-10'>
-        <div className='w-full lg:w-1/2 flex justify-center p-5'>
+        <div
+          data-aos='zoom-out'
+          data-aos-delay='1500'
+          data-aos-duration='1000'
+          data-aos-easing='ease-in-out'
+          data-aos-mirror='true'
+          data-aos-once='false'
+          className='w-full lg:w-1/2 flex justify-center p-5'
+        >
           <Image
             className='w-full object-cover rounded-lg'
             src='https://images.pexels.com/photos/7709296/pexels-photo-7709296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
@@ -80,65 +88,114 @@ const ContactPage = () => {
             className='contact-form flex flex-col gap-5'
           >
             <div className='row grid grid-cols-1 lg:grid-cols-2 gap-5'>
-              <div className='form-control w-full flex flex-col gap-2'>
-                <label htmlFor='name' className='cursor-pointer'>
-                  Name
-                </label>
-                <input
-                  className='name border border-gray-300 focus:border-gray-600 px-4 py-3 rounded-lg outline-none duration-300'
-                  type='text'
-                  name='name'
-                  id='name'
-                  placeholder='Sarah Parker'
-                  required
-                />
+              <div className='overflow-hidden'>
+                <div
+                  data-aos='fade-right'
+                  data-aos-delay='2000'
+                  data-aos-duration='1000'
+                  data-aos-easing='ease-in-out'
+                  data-aos-mirror='true'
+                  data-aos-once='false'
+                  data-aos-anchor='.row'
+                  className='form-control w-full flex flex-col gap-2'
+                >
+                  <label htmlFor='name' className='cursor-pointer'>
+                    Name
+                  </label>
+                  <input
+                    className='name border border-gray-300 focus:border-gray-600 px-4 py-3 rounded-lg outline-none duration-300'
+                    type='text'
+                    name='name'
+                    id='name'
+                    placeholder='Sarah Parker'
+                    required
+                  />
+                </div>
               </div>
 
-              <div className='form-control flex flex-col gap-2'>
-                <label htmlFor='number' className='cursor-pointer'>
-                  Phone Number
+              <div className='overflow-hidden'>
+                <div
+                  data-aos='fade-left'
+                  data-aos-delay='2000'
+                  data-aos-duration='1000'
+                  data-aos-anchor='.row'
+                  data-aos-easing='ease-in-out'
+                  data-aos-mirror='true'
+                  data-aos-once='false'
+                  className='form-control flex flex-col gap-2'
+                >
+                  <label htmlFor='number' className='cursor-pointer'>
+                    Phone Number
+                  </label>
+                  <input
+                    className='number border border-gray-300 focus:border-gray-600 px-4 py-3 rounded-lg outline-none duration-300'
+                    type='tel'
+                    name='number'
+                    id='number'
+                    placeholder='+88 01XXXXXXXXX'
+                    required
+                  />
+                </div>
+              </div>
+            </div>
+            <div className='overflow-hidden email-ctrl'>
+              <div
+                data-aos='fade-down'
+                data-aos-delay='2000'
+                data-aos-duration='1000'
+                data-aos-easing='ease-in-out'
+                data-aos-mirror='true'
+                data-aos-anchor='.email-ctrl'
+                data-aos-once='false'
+                className='form-control flex flex-col gap-2'
+              >
+                <label htmlFor='email' className='cursor-pointer '>
+                  Email Address
                 </label>
                 <input
-                  className='number border border-gray-300 focus:border-gray-600 px-4 py-3 rounded-lg outline-none duration-300'
-                  type='tel'
-                  name='number'
-                  id='number'
-                  placeholder='+88 01XXXXXXXXX'
+                  className='email border border-gray-300 focus:border-gray-600 px-4 py-3 rounded-lg outline-none duration-300'
+                  type='email'
+                  name='email'
+                  id='email'
+                  placeholder='sara@example.com'
                   required
                 />
               </div>
             </div>
 
-            <div className='form-control flex flex-col gap-2'>
-              <label htmlFor='email' className='cursor-pointer '>
-                Email Address
-              </label>
-              <input
-                className='email border border-gray-300 focus:border-gray-600 px-4 py-3 rounded-lg outline-none duration-300'
-                type='email'
-                name='email'
-                id='email'
-                placeholder='sara@example.com'
-                required
-              />
-            </div>
-
-            <div className='form-control flex flex-col gap-2'>
-              <label htmlFor='message' className='cursor-pointer'>
-                Message
-              </label>
-              <textarea
-                className='message border border-gray-300 focus:border-gray-600 h-32 rounded-xl outline-none py-3 px-4 resize-none duration-300'
-                placeholder='Type your message here...'
-                name='message'
-                id='message'
-                rows='1'
-                cols='50'
-                required
-              />
+            <div className='overflow-hidden message-ctrl'>
+              <div
+                data-aos='fade-down'
+                data-aos-duration='1000'
+                data-aos-easing='ease-in-out'
+                data-aos-mirror='true'
+                data-aos-once='false'
+                data-aos-anchor='.message-ctrl'
+                className='form-control flex flex-col gap-2'
+              >
+                <label htmlFor='message' className='cursor-pointer'>
+                  Message
+                </label>
+                <textarea
+                  className='message border border-gray-300 focus:border-gray-600 h-32 rounded-xl outline-none py-3 px-4 resize-none duration-300'
+                  placeholder='Type your message here...'
+                  name='message'
+                  id='message'
+                  rows='1'
+                  cols='50'
+                  required
+                />
+              </div>
             </div>
 
             <button
+              data-aos='fade-up'
+              data-aos-delay='2000'
+              data-aos-duration='1000'
+              data-aos-easing='ease-in-out'
+              data-aos-mirror='true'
+              data-aos-once='false'
+              data-aos-anchor='.message'
               type='submit'
               className='submit px-4 py-4 rounded-xl text-white bg-black hover:bg-gray-700 duration-300 flex items-center justify-center gap-2 overflow-hidden'
             >
