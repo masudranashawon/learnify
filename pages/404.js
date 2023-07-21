70dddec8a0dfa49fd473ae2d760abec906871502
masudranashawon <mohammadmasudrana3396@gmail.com>
2023-07-22 00:55:55 +0600

    not found page animation added

--- pages/404.js
+++ pages/404.js
@@ -4,11 +4,31 @@ const NotFoundPage = () => {
   return (
     <section className='not-found wrapper py-10 md:py-20 h-screen min-h-screen w-screen 2xl:h-[calc(100vh-6rem)]'>
       <div className='flex flex-col gap-5 items-center justify-center h-full w-full'>
-        <h1 className='text-6xl text-gray-900 font-bold lg:text-8xl'>404</h1>
-        <p className='text-xl text-center'>
+        <h1
+          data-aos='zoom-in-down'
+          data-aos-duration='1000'
+          data-aos-easing='ease-in-out'
+          className='text-6xl text-gray-900 font-bold lg:text-8xl'
+        >
+          404
+        </h1>
+        <p
+          data-aos='fade-down'
+          data-aos-delay='500'
+          data-aos-duration='1000'
+          data-aos-easing='ease-in-out'
+          className='text-xl text-center'
+        >
           {"Oops! The page you are looking for doesn't exist."}
         </p>
-        <Button href='/' placeholder='Back to Home' color='danger' />
+        <div
+          data-aos='zoom-in'
+          data-aos-delay='1000'
+          data-aos-duration='1000'
+          data-aos-easing='ease-in-out'
+        >
+          <Button href='/' placeholder='Back to Home' color='danger' />
+        </div>
       </div>
     </section>
   );
