221846e54a9c07dc4a878fee4a6d86f909c51d11
masudranashawon <mohammadmasudrana3396@gmail.com>
2023-07-21 20:08:47 +0600

    section heading animation done

--- components/SectionHeader.jsx
+++ components/SectionHeader.jsx
@@ -1,11 +1,38 @@
 const SectionHeader = ({ subHeading, heading, pera }) => {
   return (
     <div className='flex flex-col items-center text-center gap-1'>
-      <span className='uppercase text-sm font-bold tracking-widest'>
-        {subHeading}
-      </span>
-      <h2 className='text-3xl'>{heading}</h2>
-      <p className='w-3/5 text-gray-500'>{pera}</p>
+      <div className='overflow-hidden'>
+        <p
+          data-aos='slide-down'
+          data-aos-duration='1000'
+          data-aos-easing='ease-in-out'
+          className='uppercase text-sm font-bold tracking-widest'
+        >
+          {subHeading}
+        </p>
+      </div>
+      <div className='overflow-hidden'>
+        <h2
+          data-aos='slide-down'
+          data-aos-delay='500'
+          data-aos-duration='1000'
+          data-aos-easing='ease-in-out'
+          className='text-3xl'
+        >
+          {heading}
+        </h2>
+      </div>
+      <div className='overflow-hidden w-3/5'>
+        <p
+          data-aos='slide-up'
+          data-aos-delay='1000'
+          data-aos-duration='1000'
+          data-aos-easing='ease-in-out'
+          className='text-center text-gray-500'
+        >
+          {pera}
+        </p>
+      </div>
     </div>
   );
 };
